e7d27124534230ef557d67eb9c2432643169bcab
Jeff Casimir <jeff@casimircreative.com>
2011-01-22 13:24:58 -0500

    Refactoring to TagsHelper

---
+++ app/helpers/tags_helper.rb
@@ -0,0 +1,19 @@
+module TagsHelper
+  def tag_links(tags)
+    content_tag(:ul, tag_list_items(tags) + content_tag(:li, link_to("all", articles_path)))
+  end
+  
+  def tag_list_items(tags)
+    tags.collect do |tag|
+      content_tag(:li, link_to(highlighted_tag_name(tag.name), articles_path(:tag => tag.name)))
+    end.join.html_safe
+  end
+
+  def highlighted_tag_name(tag)
+    if params[:tag] && (params[:tag] == tag)
+      "<em>#{tag}</em>".html_safe
+    else
+      tag
+    end
+  end
+end
