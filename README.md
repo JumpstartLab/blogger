40b37ccaad93287fd2d308674835f102f9fdc766
Steve Klabnik <steve@steveklabnik.com>
2012-04-12 20:12:17 -0400

    Gotta love rails new

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the reference implementation of Jumpstart's Blogger project.
+
+If you'd like to build it yourself, follow along [here](http://tutorials.jumpstartlab.com/projects/blogger.html).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '1f2ae6142531fe3821d5f48fd7b525e13a0b815b8fba409d24a7ce8b8848f9552db99a53ca4c0950291be02986203717255ed971f49c9e7225c476575652670f'
