2ab8ac984cba8bf11307acd352cb760a76941756
Steve Klabnik <steve@steveklabnik.com>
2012-04-12 20:49:14 -0400

    Iteration 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,37 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+
+    flash[:message] = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
 end
